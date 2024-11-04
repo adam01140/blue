bd19ccad431d7a9bb7a4e0cf70a4a0c7e2fc133d
achaaban <achaaban@ucsc.edu>
2024-11-03 19:57:54 -0800

    q

--- server.js
+++ server.js
@@ -52,7 +52,6 @@ app.post('/send-email', async (req, res) => {
     }
 });
 
-// Endpoint to add user
 app.post('/addUser', async (req, res) => {
   const { name, email } = req.body;
   if (!name || !email) {
@@ -76,57 +75,7 @@ app.post('/upload', async (req, res) => {
   res.json({ message: 'PDF uploaded successfully.', fields: [] });
 });
 
-// List of field names that exist in the PDF
-
-
-//put textbox names here and checkmarks
-const pdfFieldNames = [
-  'SC-100[0].Page2[0].List1[0].Item1[0].PlaintiffName1[0]','PetitionerName1[0]', 'County', 'Street', 'case_number', 'PetitionerCity[0]', 'PetitionerState[0]',
-  'a1','b1','SC-100[0].Page2[0].List1[0].Item1[0].PlaintiffAddress1[0]',
-  'PetitionerZip[0]', 'PetitionerTel[0]', 'current_date', 'monthly_income_source1', 'lawyer_info', 
-  'lawyercheckyes', 'lawyercheckno', 'job_title', 'employer_name', 'job', 'employer_address', 
-  'superior_court_checkbox', 'supreme_court_checkbox', 'food_stamps', 'supp_sec_inc', 'ssp', 
-  'medical', 'county_relief', 'calworks', 'capi', 'ihss', 'wic', 'unemployment', 'low_gross_income',
-  'waive_some', 'pay_later', 'waive_within_6months', 'previous_available', 'changing_income_checkbox',
-  'monthly_income_source1_amount', 'monthly_income_source2', 'monthly_income_source2_amount',
-  'monthly_income_source3', 'monthly_income_source3_amount', 'monthly_income_source4', 
-  'monthly_income_source4_amount',
-  'not_enough', 'waive_all', 'dependency_name1', 'dependency_age1', 'dependency_relationship1',
-  'dependency_income1', 'dependency_name2', 'dependency_age2', 'dependency_relationship2',
-  'dependency_income2', 'dependency_name3', 'dependency_age3', 'dependency_relationship3',
-  'dependency_income3', 'dependency_name4', 'dependency_age4', 'dependency_relationship4',
-  'dependency_income4', 'dependency_income_total', 'monthly_income_total', 'total_combined_income',
-  'cash_amount', 'bank_name1', 'bank_amount1', 'bank_name2', 'bank_amount2', 'bank_name3', 'bank_amount3',
-  'vehicle1_make_year', 'vehicle1_market_value', 'vehicle1_amount_owed', 'vehicle2_make_year', 
-  'vehicle2_market_value', 'vehicle2_amount_owed', 'vehicle3_make_year', 'vehicle3_market_value', 
-  'vehicle3_amount_owed', 
-  'real_estate1_address', 'real_estate1_market_value', 'real_estate1_amount_owed',
-  'real_estate2_address', 'real_estate2_market_value', 'real_estate2_amount_owed', 
-  'property1_description','property1_market_value', 'property1_amount_owed', 'property2_description', 'property2_market_value',
-  'property2_amount_owed', 
-  'payroll_description1', 'payroll_deduction1', 'payroll_description2', 
-  'payroll_deduction2', 'payroll_description3', 'payroll_deduction3', 'payroll_description4', 
-  'payroll_deduction4', 
-  'rent_expense', 'food_expense', 'utilities_expense', 'clothing_expense', 
-  'laundry_expense', 'medical_expense', 'insurance_expense', 'school_childcare_expense', 'support_expense',
-  'transportation_expense', 
-  'installment_payment1', 'installment_payment2', 'installment_payment3', 
-  'installment_payment1_amount', 'installment_payment2_amount', 'installment_payment3_amount',
-  'withheld_earnings', 
-  'other_expense1', 'other_expense2', 'other_expense3', 
-  'other_expense1_amount','other_expense2_amount','other_expense3_amount',
-  'total_monthly_expenses'
-];
-
-
-
-
-
-
-
-// Endpoint to edit PDF
 app.post('/edit_pdf', async (req, res) => {
-  // Check if the PDF file is included in the request and is not empty
   if (!req.files || !req.files.pdf) {
     return res.status(400).send('No PDF file uploaded.');
   }
@@ -140,39 +89,12 @@ app.post('/edit_pdf', async (req, res) => {
   const form = pdfDoc.getForm();
   const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
 
-  // Log all field names in the PDF
-  const fields = form.getFields();
-  fields.forEach(field => {
-    console.log(`Field name: ${field.getName()}`);
-  });
-  
-  // Process only the fields that exist in the PDF
   pdfFieldNames.forEach(key => {
     if (req.body[key] !== undefined) {
       try {
-        if (key === 'lawyer_info') {
-          const field = form.getTextField(key);
+        const field = form.getTextField(key);
+        if (field) {
           field.setText(req.body[key]);
-          field.updateAppearances(helveticaFont);
-        } else if (['lawyercheckyes', 'lawyercheckno', 'superior_court_checkbox', 'supreme_court_checkbox', 'low_gross_income', 'waive_all', 'not_enough', 'waive_some', 'pay_later', 'waive_within_6months', 'previous_available', 'changing_income_checkbox'].includes(key)) {
-          const field = form.getCheckBox(key);
-          if (req.body[key] === 'Yes') {
-            field.check();
-          } else {
-            field.uncheck();
-          }
-        } else if (['food_stamps', 'supp_sec_inc', 'ssp', 'medical', 'county_relief', 'calworks', 'capi', 'ihss', 'wic', 'unemployment'].includes(key)) {
-          const field = form.getCheckBox(key);
-          if (req.body[key] === 'Yes') {
-            field.check();
-          } else {
-            field.uncheck();
-          }
-        } else {
-          const field = form.getTextField(key);
-          if (field) {
-            field.setText(req.body[key]);
-          }
         }
       } catch (e) {
         console.error(`No such field in the PDF: ${key}`);
@@ -188,13 +110,13 @@ app.post('/edit_pdf', async (req, res) => {
   res.send(Buffer.from(updatedPdfBytes));
 });
 
+// Serve static PDF files
+app.get('/form1.pdf', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/form1.pdf'));
+});
 
-
-
-
-// Serve static PDF file
-app.get('/form.pdf', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/form.pdf'));
+app.get('/form2.pdf', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/form2.pdf'));
 });
 
 const PORT = process.env.PORT || 3000;
